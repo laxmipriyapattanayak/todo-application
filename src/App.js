513f5a7584663e251cd8a251dd3ce79bb329f53d
laxmipriyapattanayak <cma.cma89@gmail.com>
2024-03-28 22:08:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    console.log(id);
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleUpdateItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, update: !item.update } : item
+      )
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form onAddItems={handleAddItems} />
+      <TodoItemList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onUpdateItem={handleUpdateItem}
+      />
     </div>
   );
 }
 
-export default App;
+function Form({ onAddItems }) {
+  const [dateValue, setDateChange] = useState(new Date());
+  const [inputItem, setInputItem] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(dateValue);
+    console.log(inputItem);
+    const newItem = { dateValue, inputItem, id: Date.now(), update: false };
+    onAddItems(newItem);
+    console.log(newItem);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Select date</label>
+      <input
+        type="date"
+        value={dateValue}
+        onChange={(e) => setDateChange(e.target.value)}
+      />
+      <label>todolist</label>
+      <input
+        type="text"
+        placeholder="item name"
+        value={inputItem}
+        onChange={(e) => setInputItem(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+function TodoItemList({ items, onDeleteItem, onUpdateItem }) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <Item
+          item={item}
+          key={item.id}
+          onDeleteItem={onDeleteItem}
+          onUpdateItem={onUpdateItem}
+        />
+      ))}
+    </ul>
+  );
+}
+function Item({ item, onDeleteItem, onUpdateItem }) {
+  return (
+    <>
+      <li>
+        {item.dateValue}
+        {item.update ? (
+          <input
+            type="text"
+            value={item.inputItem}
+            onChange={(e) => console.log(e.target.value)}
+          />
+        ) : (
+          item.inputItem
+        )}
+        <button onClick={() => onDeleteItem(item.id)}>remove</button>
+        <button onClick={() => onUpdateItem(item.id)}>Update</button>
+      </li>
+    </>
+  );
+}
